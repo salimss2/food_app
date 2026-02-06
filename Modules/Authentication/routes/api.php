1896be192a8f84a0c2e8d8c4a5b13e1d47dc1957
salimss2 <you@example.com>
2026-02-06 23:53:30 +0300

    Adding registration tools to the identity module

---
+++ Modules/Authentication/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Authentication\Http\Controllers\AuthenticationController;
+use Modules\Authentication\app\Http\Controllers\AuthenticationController;
+
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::apiResource('authentications', AuthenticationController::class)->names('authentication');
+});
+
+
+Route::post('/register', [AuthenticationController::class, 'register']);
